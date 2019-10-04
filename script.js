d88d370ae6ab20c403beb3892ff0f6bacf6581d9
marumo-sound <marumo.sound.system.engineer@gmail.com>
2019-10-04 20:50:51 +0900

    aaaa

--- script.js
+++ script.js
@@ -1,14 +1,12 @@
 $(function(){
-  
+
   // 左メニュー押下時のページ内遷移
-  $('header a').click(function() {
-    
+  $('.left-wrapper a').click(function() {
     var id =  $(this).attr('href');
     var position = $(id).offset().top;
-    
     $('html, body').animate({
       'scrollTop': position
-    }, 500);
+    }, 'slow');
   });
 
 
