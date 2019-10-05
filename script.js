cdbbb686e1af9148b3e7e87f44e24c4ecd55b5f5
marumo-sound <marumo.sound.system.engineer@gmail.com>
2019-10-05 14:28:58 +0900

    コミット

--- script.js
+++ script.js
@@ -1,5 +1,29 @@
-$(function(){
-
+$(function() {
+  
+  WebFont.load({
+    google: {
+      families: ['Noto Sans JP', 'Share Tech Mono']
+    },
+    loading: function() {
+      // window.alert('loading');
+    },
+    active: function() {
+      // window.alert('active');
+    },
+    inactive: function() {
+      // window.alert('inactive');
+    },
+    fontloading: function(familyName, fvd) {
+      // window.alert('fontloading' + ' ' + familyName + ' ' + fvd);
+    },
+    fontactive: function(familyName, fvd) {
+      // window.alert('fontactive' + familyName + fvd);
+    },
+    fontinactive: function(familyName, fvd) {
+      // window.alert('fontinactive' + familyName + fvd);
+    }
+  });
+  
   // 左メニュー押下時のページ内遷移
   $('.left-wrapper a').click(function() {
     var id =  $(this).attr('href');
@@ -8,6 +32,4 @@ $(function(){
       'scrollTop': position
     }, 'slow');
   });
-
-
 });
